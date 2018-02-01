18a13d14779747da86ac81f6ac7db4fb5e84697f
David Lee <daushiang@gmail.com>
2018-02-01 16:37:11 +0800

    Simple http server

--- ruby/httpSrv/tcp_srv.rb
+++ ruby/httpSrv/tcp_srv.rb
@@ -3,6 +3,13 @@ require 'socket'
 server = TCPServer.new 5678
 
 while session = server.accept
-  session.puts "Hello World! The time is #{Time.now}"
+  request = session.gets
+  puts request
+
+  session.print "HTTP/1.1 200\r\n" 
+  session.print "Content-Type: text/html\r\n" 
+  session.print "\r\n"  
+  session.print "Hello world! The time is #{Time.now}"  
+
   session.close
 end
