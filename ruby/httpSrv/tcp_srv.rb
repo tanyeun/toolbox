165109c41d46058953af30e0618f0dc121cda91c
David Lee <daushiang@gmail.com>
2018-02-01 16:40:19 +0800

    Add comments

--- ruby/httpSrv/tcp_srv.rb
+++ ruby/httpSrv/tcp_srv.rb
@@ -6,10 +6,10 @@ while session = server.accept
   request = session.gets
   puts request
 
-  session.print "HTTP/1.1 200\r\n" 
-  session.print "Content-Type: text/html\r\n" 
-  session.print "\r\n"  
-  session.print "Hello world! The time is #{Time.now}"  
+  session.print "HTTP/1.1 200\r\n"                      # Status line: Protocal & Response status to the browser
+  session.print "Content-Type: text/html\r\n"           # Header: Indicate the response content type
+  session.print "\r\n"    
+  session.print "Hello world! The time is #{Time.now}"  # The message body 
 
   session.close
 end
