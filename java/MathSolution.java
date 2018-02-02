d6322170e3e69445ee57421ec52b555d1f36145c
David Lee <daushiang@gmail.com>
2018-02-03 05:16:52 +0800

    getMaxMoviesWatched

--- java/MathSolution.java
+++ java/MathSolution.java
@@ -6,6 +6,13 @@ class MathSolution {
 	     System.out.println("##### Get Max Distance Square #####");	
        System.out.println(getMaxDistanceSquare(2,4,2,4));
        System.out.println(getMaxDistanceSquare(1,2,3,4));
+
+       System.out.println();
+		   int[] movies1 = {6, 1, 4, 6, 3, 2, 7, 4};
+		   int[] movies2 = {10, 19, 15};
+	     System.out.println("##### Get Max Distance Square #####");	
+       System.out.println(getMaxMoviesWatched(movies1, 3, 2));
+       System.out.println(getMaxMoviesWatched(movies2, 2, 2));
      }
   }
   
@@ -22,4 +29,39 @@ class MathSolution {
     return max;
   }
 
+  static int getMaxMoviesWatched(int[] movies, int K, int L){
+    int length = movies.length;
+    if ( length < K+L )
+      return -1;
+
+    int maxK = 0;
+    int indK = 0;
+    for(int i = 0; i <= length-K; i++){
+      int value = 0;
+      for(int j = i; j < K+i; j++)
+        value += movies[j];
+
+      if ( value > maxK ){
+        maxK = value;
+        indK = i;
+      }
+    }
+
+    int maxL = 0;
+    for(int i = 0; i <= length-L; i++){
+      int value = 0;
+      if( i == indK )
+        i += K;
+
+      for(int j = i; j < L+i; j++)
+        value += movies[j];
+
+      if ( value > maxL ){
+        maxL = value;
+      }
+    }
+
+    return maxK + maxL;
+
+  }
 }
